ea0de7d602ab108bbb2f669a7530861ea9a82b47
Tiago Januário <tiagojdsa@gmail.com>
2020-12-15 15:22:49 -0300

    aplicando javaScript
    aplicando açoes na aba de produto com JavaScript

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,15 @@
-function exibir_categoria(categoria){
+function exibir_categoria(categorias){
     //alert(categoria);
 
     let elementos = document.getElementsByClassName('box-produto');
     console.log(elementos);
+    for(var i=0; i<elementos.length; i++){
+        console.log(elementos[i].id);
 
+        if(categorias == elementos[i].id)
+            elementos[i].id = "display:block";
+        else 
+            elementos[i].style = "display:none";    
+
+    }
 }
