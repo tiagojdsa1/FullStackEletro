2b2e89dc64e57f8249beedd20e7a4fad4aa1b04c
Tiago Januário <tiagojdsa@gmail.com>
2020-12-15 16:43:18 -0300

    upgrade interaçao

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,29 @@
-function exibir_categoria(categorias){
+function exibir_categorias(categorias){
     //alert(categoria);
 
     let elementos = document.getElementsByClassName('box-produto');
     console.log(elementos);
     for(var i=0; i<elementos.length; i++){
         console.log(elementos[i].id);
-
         if(categorias == elementos[i].id)
-            elementos[i].id = "display:block";
+            elementos[i].style = "display:block";
         else 
-            elementos[i].style = "display:none";    
-
+            elementos[i].style = "display:none";
     }
 }
+
+let exibir_todas = () => {
+    let elementos = document.getElementsByClassName('box-produto');
+    
+    for(var i=0; i<elementos.length; i++){ 
+        elementos[i].style = "display:block";        
+    }
+};
+
+let destaque = (imagem) => {
+    console.log(imagem);
+    if(imagem.width == 240)
+    imagem.width = 120;
+    else
+    imagem.width = 240;
+};
